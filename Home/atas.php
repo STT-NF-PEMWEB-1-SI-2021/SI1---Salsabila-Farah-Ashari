e72f384ad07a5b756d43eaa622316ac61c38839f
Salsabila Farah Ashari <91241523+Farahashari@users.noreply.github.com>
2022-03-14 21:02:52 +0700

    Add files via upload

---
+++ Home/atas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <title>SAS-STTNF</title>
+</head>
+
+<body>
+
+<header role="banner">
+    <h2>Student Activity Score - STTNF</h2>
+    <hr>
+</header>
+
